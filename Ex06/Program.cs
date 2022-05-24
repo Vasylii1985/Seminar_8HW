64cdc6b235e5c9edf65bd6f3486b453f0814d3ac
Vasilii <bespalchuk1985@gmail.com>
2022-05-24 13:59:37 +0300

    Ex06

---
+++ Ex06/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 4;
+int[,] sqareMatrix = new int[n, n];
+int[,] array = new int[n, n];
+int number = 1;
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n - i; j++) array[i, j] = number++;
+    for (int k = i + 1; k < n - i; k++) array[k, n - 1 - i] = number++;
+    for (int j = n - 2 - i; j >= i; j--) array[n - 1 - i, j] = number++;
+    for (int k = n - 2 - i; k > i; k--) array[k, i] = number++;
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}    
